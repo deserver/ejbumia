6c38f7c7cb55d1c8d9935d76173ae0ea0862dbde
sergio <deserver123@gmail.com>
2016-03-11 22:19:40 -0300

    First commit

---
+++ EjbJaxRS/web/src/main/java/py/pol/una/ii/pw/controller/ProductController.java
@@ -0,0 +1,134 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package py.pol.una.ii.pw.controller;
+
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.enterprise.inject.Produces;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import py.pol.una.ii.pw.model.Product;
+import py.pol.una.ii.pw.service.ProductRegistration;
+
+// The @Model stereotype is a convenience mechanism to make this a request-scoped bean that has an
+// EL name
+// Read more about the @Model stereotype in this FAQ:
+// http://sfwk.org/Documentation/WhatIsThePurposeOfTheModelAnnotation
+@Model
+public class ProductController {
+
+    @Inject
+    private FacesContext facesContext;
+    
+    @Inject
+    private Logger log;
+
+    @Inject
+    private ProductRegistration productRegistration;
+
+    private Product newProduct;
+
+    @Produces
+    @Named
+    public Product getNewProduct() {
+        return newProduct;
+    }
+
+    public void register() throws Exception {
+		try {
+            productRegistration.register(newProduct);
+            facesContext.addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful"));
+            initNewProduct();
+        } catch (Exception e) {
+            String errorMessage = getRootErrorMessage(e);
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Registration Unsuccessful");
+            facesContext.addMessage(null, m);
+        }
+    }
+    
+    public void delete(Long id) throws Exception {
+    	try{
+    		productRegistration.delete(id);
+    		facesContext.addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Deleted!", "Delete successful"));
+    	}catch (Exception e){
+    		String errorMessage = getRootErrorMessage(e);
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Registration Unsuccessful");
+            facesContext.addMessage(null, m);
+    	}
+    }
+    
+    public void modify(Long id) throws Exception{
+    	try{
+    		newProduct = productRegistration.getProduct(id);
+    		//newProduct.setId(modProduct.getId());
+    		//newProduct.setCantidad(modProduct.getCantidad());
+    		//newProduct.setName(modProduct.getName());
+    	}catch (Exception e){
+    		String errorMessage = getRootErrorMessage(e);
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Registration Unsuccessful");
+            facesContext.addMessage(null, m);
+    	}
+    }
+    
+    public void registerMod(Long id) throws Exception{
+    	try {
+    		newProduct.setId(id);
+            productRegistration.update(newProduct);
+            facesContext.addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO, "Modified!", "Modification successful"));
+            initNewProduct();
+        } catch (Exception e) {
+            String errorMessage = getRootErrorMessage(e);
+            FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Registration Unsuccessful");
+            facesContext.addMessage(null, m);
+        }
+    }
+
+    @PostConstruct
+    public void initNewProduct() {
+        newProduct = new Product();
+    }
+    
+    
+    
+
+    private String getRootErrorMessage(Exception e) {
+        // Default to general error message that registration failed.
+        String errorMessage = "Registration failed. See server log for more information";
+        if (e == null) {
+            // This shouldn't happen, but return the default messages
+            return errorMessage;
+        }
+
+        // Start with the exception and recurse to find the root cause
+        Throwable t = e;
+        while (t != null) {
+            // Get the message from the Throwable class instance
+            errorMessage = t.getLocalizedMessage();
+            t = t.getCause();
+        }
+        // This is the root cause message
+        return errorMessage;
+    }
+}
